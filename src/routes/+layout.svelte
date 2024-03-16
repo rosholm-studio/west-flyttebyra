48307139948db498b908bf1b2141c78270af315c
Seïn Bucher <sein.bucher@gmail.com>
2024-03-16 11:08:11 +0100

    google tag manager

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,4 +1,15 @@
-<script>;
+<svelte:head>
+    <!-- Google Tag Manager -->
+    <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+    new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+    j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+    'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+    })(window,document,'script','dataLayer','GTM-W5DF8XP5');</script>
+    <!-- End Google Tag Manager -->
+</svelte:head>
+
+<script>
+    
     import "../global.css";
     import Navbar from "$lib/Navbar.svelte";
     import Footer from "$lib/Footer.svelte";
